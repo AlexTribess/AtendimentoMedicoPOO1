b3ca3e4b218750586be8fb3b54dde08c633534e9
Victor Hugo Reinicke <pombewcraft@gmail.com>
2025-10-27 17:31:49 -0300

    Classificacao criada sem erros

---
+++ Classificacao.java
@@ -0,0 +1,39 @@
+import java.time.Duration;
+
+public class Classificacao {
+
+	private int cor;
+	private String descricao;
+	private Duration tempoEspera;
+
+	public Classificacao(int cor, String descricao, Duration tempoEspera) {
+		super();
+		this.cor = cor;
+		this.descricao = descricao;
+		this.tempoEspera = tempoEspera;
+	}
+
+	public int getCor() {
+		return cor;
+	}
+
+	public void setCor(int cor) {
+		this.cor = cor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Duration getTempoEspera() {
+		return tempoEspera;
+	}
+
+	public void setTempoEspera(Duration tempoEspera) {
+		this.tempoEspera = tempoEspera;
+	}
+}
